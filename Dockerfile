6491d457b0aa1a829b3882fdb5664bcc0265e5fd
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-01-15 16:32:46 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM node:16.13.2-bullseye-slim
 
-ENV TINI_VERSION v0.19.0
-ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
-RUN chmod +x /tini
-ENTRYPOINT ["/tini", "--"]
-
 WORKDIR /opt/app
 RUN chown node:node ./
 USER node
@@ -12,7 +7,7 @@ USER node
 ARG NODE_ENV=production
 ENV NODE_ENV $NODE_ENV
 
-COPY package*.json ./
+COPY --chown=node:node package.json  package-lock.json ./
 RUN npm ci && npm cache clean --force
 
 COPY --chown=node:node . .
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import 'source-map-support/register';
 import {createServer} from 'http';
 import axios from 'axios';
+import {Socket} from 'net';
 
-const DEFAULT_PORT = '3000';
-const PORT = parseInt(process.env.PORT ?? DEFAULT_PORT);
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT ?? DEFAULT_PORT;
 
 let count = 1;
 
@@ -13,14 +14,14 @@ const server = createServer(async (req, res) => {
 
   switch (path) {
     case '/ping': {
+      console.log({count});
       res.write(JSON.stringify({pong: count++}));
-      console.log(`count: ${count}`);
       break;
     }
     case '/ip': {
       const {data} = await axios.get('https://checkip.amazonaws.com');
       const ip = data.trim('\n');
-      res.write({ip});
+      res.write(JSON.stringify({ip}));
       break;
     }
     default: {
@@ -32,16 +33,49 @@ const server = createServer(async (req, res) => {
   res.end();
 });
 
-server.listen(PORT);
+const shutdown = async () => {
+  // server.closeを呼び出すと、新規のコネクションの受け付けを終了し、既存のコネクションが終了するとコールバック関数を実行する。
+  // しかし、keepaliveが有効であれば既存のコネクションが使い回されるのですぐにコネクションが終了しない。
+  server.close(err => {
+    if (err !== undefined) {
+      console.error(err);
+      // eslint-disable-next-line no-process-exit
+      process.exit(1);
+    }
 
-console.log('app is running!');
+    console.log('terminate server.');
+    // eslint-disable-next-line no-process-exit
+    process.exit();
+  });
+
+  // 一定期間経過後にコネクションを終了する。
+  setTimeout(() => {
+    sockets.forEach(socket => {
+      socket.destroy();
+      sockets.delete(socket);
+    });
+  }, 3000);
+};
+
+// シャットダウン処理の為にコネクションをSetで管理しておく。
+const sockets = new Set<Socket>();
+server.on('connection', socket => {
+  sockets.add(socket);
+
+  socket.once('close', () => {
+    sockets.delete(socket);
+  });
+});
 
 process.once('SIGTERM', async () => {
-  console.log('SIGTERM received, terminate server.');
-  server.close();
+  console.log('SIGTERM received.');
+  shutdown();
 });
 
 process.once('SIGINT', async () => {
-  console.log('SIGINT received, terminate server.');
-  server.close();
+  console.log('SIGINT received.');
+  shutdown();
 });
+
+server.listen(PORT);
+console.log('app is running!');
